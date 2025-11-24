9facae076a5b082ea171c22943066aa94a6a21bd
Kamal <elesgerlikamal04@gmail.com>
2025-11-23 19:30:49 -0500

    feat: bar colors, title

--- src/2d/bar.cpp
+++ src/2d/bar.cpp
@@ -8,64 +8,68 @@ namespace graphlib {
 void BarPlot::show() const {
     const int width = 800;
     const int height = 600;
-    InitWindow(width, height, "Bar Plot Example");
+    InitWindow(width, height, title.c_str());
     SetTargetFPS(60);
 
-    // Padding around plot
     const int left_padding = 100;
     const int right_padding = 50;
-    const int top_padding = 50;
+    const int top_padding = 100;   // more space for title
     const int bottom_padding = 80;
 
-    // Number of bars
     size_t n_bars = std::max<size_t>(1, x_labels.size());
-
-    // Compute bar width
     int plot_width = width - left_padding - right_padding;
     int bar_width = plot_width / static_cast<int>(n_bars);
-
-    // Find max y for scaling
     double max_y = *std::max_element(y_values.begin(), y_values.end());
-    if (max_y == 0) max_y = 1; // avoid division by zero
+    if (max_y == 0) max_y = 1;
 
     while (!WindowShouldClose()) {
         BeginDrawing();
         ClearBackground(RAYWHITE);
 
+        int plot_bottom = height - bottom_padding;
+        int plot_height = height - top_padding - bottom_padding;
+        int plot_left = left_padding;
+
+        // ----------------------
+        // Draw title
+        // ----------------------
+        DrawText(title.c_str(), width/2 - MeasureText(title.c_str(), 30)/2, 20, 30, BLACK);
+
         // ----------------------
         // Draw y-axis
         // ----------------------
-        DrawLine(left_padding, top_padding, left_padding, height - bottom_padding, BLACK);
+        DrawLine(plot_left, top_padding, plot_left, plot_bottom, BLACK);
 
-        // Draw y-axis labels (5 divisions)
         int n_div = 5;
         for (int i = 0; i <= n_div; ++i) {
             double y_val = max_y * i / n_div;
-            int y_px = static_cast<int>(height - bottom_padding - (y_val / max_y) * (height - top_padding - bottom_padding));
-            DrawLine(left_padding - 5, y_px, left_padding, y_px, BLACK);
+            int y_px = static_cast<int>(plot_bottom - (y_val / max_y) * plot_height);
+            DrawLine(plot_left - 5, y_px, plot_left, y_px, BLACK);
             DrawText(std::to_string(static_cast<int>(y_val)).c_str(),
-                     left_padding - 50, y_px - 10, 20, BLACK);
+                     plot_left - 50, y_px - 10, 20, BLACK);
         }
+
+        // ----------------------
+        // Draw x-axis line
+        // ----------------------
+        DrawLine(plot_left, plot_bottom, width - right_padding, plot_bottom, BLACK);
 
         // ----------------------
         // Draw bars
         // ----------------------
-        int plot_left = left_padding;
-        int plot_bottom = height - bottom_padding;
-        int plot_height = height - top_padding - bottom_padding;
-
-        // Compute total width of all bars including spacing
         int total_bar_space = n_bars * bar_width;
-        int start_x = plot_left + (plot_width - total_bar_space) / 2; // center bars
+        int start_x = plot_left + (plot_width - total_bar_space) / 2;
 
         for (size_t i = 0; i < n_bars; ++i) {
             double scaled_height = y_values[i] / max_y * plot_height;
             int x = start_x + static_cast<int>(i * bar_width);
             int y = static_cast<int>(plot_bottom - scaled_height);
 
-            DrawRectangle(x, y, bar_width - 10, static_cast<int>(scaled_height), BLUE);
+            // Choose color: if user provided, use it; otherwise BLUE
+            Color c = (i < colors.size()) ? colors[i] : BLUE;
+            int margin = 2;
+            DrawRectangle(x, y, bar_width - 10, static_cast<int>(scaled_height) - margin, c);
 
-            // Draw labels
             DrawText(x_labels[i].c_str(), x, height - bottom_padding + 5, 20, BLACK);
         }
 
@@ -76,3 +80,4 @@ void BarPlot::show() const {
 }
 
 } // namespace graphlib
+
