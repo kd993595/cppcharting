2b98917a1c8c38eb9740e6017d1a342d25a1b621
Richard Uzeel <richard.uzeel5@gmail.com>
2025-11-30 19:03:45 -0500

    Finished pie chart

--- examples/pie_example.cpp
+++ examples/pie_example.cpp
@@ -2,7 +2,7 @@
 #include <vector>
 
 int main(){
-    std::vector<int> grades = {83,91,93,84,99,89,60,94,97,92,93,85,97,66,59,88,80,81,96,83,99,65,59,98,82,81,94,68,72,77,82};
+    std::vector<int> grades = {81,82,82,83,83,83,84,84,84,84};
     auto plot = graphlib::pie(grades.begin(), grades.end());
     plot.title = "Exam Grade Pie Chart";
     plot.color = RED;
