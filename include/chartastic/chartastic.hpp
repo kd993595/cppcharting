fd93629030ffa5b61f0fff6af72f2cfadddfbdf1
Kamal <elesgerlikamal04@gmail.com>
2025-12-03 13:10:35 -0500

    feat: main header file

---
+++ include/chartastic/chartastic.hpp
@@ -0,0 +1,25 @@
+#ifndef CHARTASTIC_HPP
+#define CHARTASTIC_HPP
+
+// Core utilities
+#include <chartastic/core/colors.hpp>
+#include <chartastic/core/concepts.hpp>
+#include <chartastic/core/error.hpp>
+
+// 2D plot base
+#include <chartastic/2d/plot.hpp>
+
+// 2D charts
+#include <chartastic/2d/area.hpp>
+#include <chartastic/2d/bar.hpp>
+#include <chartastic/2d/box.hpp>
+#include <chartastic/2d/facetgrid.hpp>
+#include <chartastic/2d/hist.hpp>
+#include <chartastic/2d/line.hpp>
+#include <chartastic/2d/pie.hpp>
+#include <chartastic/2d/scatter.hpp>
+
+// 3D charts
+#include <chartastic/3d/bar.hpp>
+
+#endif // CHARTASTIC_HPP
